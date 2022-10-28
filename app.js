e39c75c84fc4eba542be31af63f8d7ec6eaeb7d8
Arshroop Singh <arshroopsaini@gmail.com>
2022-10-29 05:18:32 +0530

    config for hosting on render

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const config = {
   authRequired: false,
   auth0Logout: true,
   secret: 'a long, randomly-generated string stored in env',
-  baseURL: 'https://woofyverse.herokuapp.com',
+  baseURL: 'https://woofyverse.onrender.com',
   clientID: 'u7vJi1WJIxdNMLWN1LAyjleCfvFR4Sta',
   issuerBaseURL: 'https://dev-hri34pn2.us.auth0.com'
 };
@@ -75,96 +75,35 @@ app.use(methodOverride('_method'))
 mongoose.connect("mongodb+srv://arshroop:Asdfjkl123@cluster0.z4k2m.mongodb.net/?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set("useCreateIndex", true);
 
-const postSchema ={
-  dogName: {
-   type: String,
-   required:true,
-   
-  },
-  duration:{
-    type: String,
-  },
-  breed:{
-    type: String,
-    required:true,
-  },
-  adopted:{
-    type: Boolean,
-    required: true,
-  },
-  date: {
-    type:Date,
-    required:true,
-  },
-  ownerName: {
-    type:String,
-    required:true,
-  },
-  shots: {
-    type:String,
-    required:true,
-  },
-  dogAge: {
-    type:String,
-    required:true,
-  },
-  spayed: {
-    type:String,
-    required:true,
-  },
-  neutered: {
-    type:String,
-    required:true,
-  },
-  vaccinated: {type:String,required:true,},
-  kids: {type:String,required:true,},
-  cats: {
-    type:String,
-    required:true,
-  },
-  dogs: {
-    type:String,
-    required:true,
-  },
-  state: {
-    type:String,
-    required:true,
-  },
-  city: {
-    type:String,
-    required:true,
-  },
-  gender: {
-    type:String,
-    required:true,
-  },
-  ownerAddress: {
-    type:String,
-    required:true,
-  },
-  ownerPhone: {
-    type:Number,
-    required:true,
-  },
-  additionalOne: {
-    type:String,
-  },
-  additionalTwo: {
-    type:String,
-  },
-imagePath: {
-  type:String,
-  required:true,
-},
-randomNumber:Number,
-};
+const postSchema ={dogName:{type: String,required:true},
+  duration:{type: String},
+  breed:{type: String,required:true},
+  adopted:{type: Boolean,required: tru},
+  date:{type:Date,required:true},
+  ownerName:{type:String,required:true},
+  shots:{type:String,required:true},
+  dogAge:{type:String,required:true},
+  spayed:{type:String,required:true},
+  neutered:{type:String,required:true},
+  vaccinated:{type:String,required:true,},
+  kids:{type:String,required:true,},
+  cats:{type:String,required:true},
+  dogs:{type:String,required:true},
+  state:{type:String,required:true},
+  city:{type:String,required:true},
+  gender:{type:String,required:true},
+  ownerAddress:{type:String,required:true},
+  ownerPhone:{type:Number,required:true},
+  additionalOne:{type:String},
+  additionalTwo:{type:String},
+imagePath:{type:String,required:true},
+randomNumber:Number};
+
 const Post = mongoose.model("Post", postSchema);
 
 app.get('/news',function (req,res) {
 
   const axios = require("axios");
-  
-
 const options = {
   method: 'GET',
   url: 'https://daily-dog-news.p.rapidapi.com/news',
@@ -173,16 +112,12 @@ const options = {
     'X-RapidAPI-Host': 'daily-dog-news.p.rapidapi.com'
   }
 };
-
 axios.request(options).then(function (response) {
   const news= response.data;
-
   var datas= []
   for (var index = 0; index < response.data.length; index++) {
     datas.push(news[index])
   }
-
-
   const length= response.data.length
   console.log(datas);
   res.render('news',{
@@ -190,7 +125,6 @@ axios.request(options).then(function (response) {
     length:length,
     text: req.oidc.isAuthenticated() ? 'LOGOUT' : 'LOGIN',
   })
-	
 }).catch(function (error) {
 	console.error(error);
 });
@@ -207,9 +141,7 @@ res.render("index", {
 })
 
 app.get('/permanent-adoption', function(req,res){
-  
   const axios = require("axios");
-
 const options = {
   method: 'GET',
   url: 'https://dog-facts2.p.rapidapi.com/facts',
@@ -218,15 +150,10 @@ const options = {
     'X-RapidAPI-Host': 'dog-facts2.p.rapidapi.com'
   }
 };
-
-
 var noMatch = null;
-
-
     if(req.query.city || req.query.state) {
         const city = new RegExp(escapeRegex(req.query.city), 'gi');
         const state= new RegExp(escapeRegex(req.query.state), 'gi');
-        
         Post.find({$and:[{state: state}, {city:city},{adopted:false},{duration:"long"}]}, function(err, posts){
            if(err){
                console.log(err);
@@ -257,7 +184,6 @@ var noMatch = null;
           axios.request(options).then(function (response) {
             const fact=response.data.facts[0];
             console.log(fact);
-        
           res.render("home", {
             posts: posts,
             noMatch: noMatch,
@@ -271,12 +197,14 @@ var noMatch = null;
     }).sort({date:"desc"});
     }
 });
+
 app.get('/copyright', function(req, res) {
 res.render('copyright',
 {
   text: req.oidc.isAuthenticated() ? 'LOGOUT' : 'LOGIN',
 })
 })
+
 app.get('/adopted', function(req, res) {
   Post.find({adopted:true}, function(err, posts){
     
@@ -292,43 +220,16 @@ app.get('/adopted', function(req, res) {
 })
 
 app.get("/compose", requiresAuth(), function(req, res){
- 
     res.render("compose",{
       text: req.oidc.isAuthenticated() ? 'LOGOUT' : 'LOGIN',
     });
 });
 
 app.post("/compose",requiresAuth(), function(req, res){
-  
   const file = req.files.imageOne;
   cloudinary.uploader.upload(file.tempFilePath,(err,result)=>{
-    
     console.log(result);
-    const post = new Post({
-      randomNumber: Math.floor(Math.random()*1000000000000000),
-      dogName: req.body.dogName,
-      breed: req.body.breed,
-      duration: req.body.type,
-      date: req.body.postDate,
-      dogAge: req.body.dogAge,
-      adopted: false,
-      shots: req.body.shots,
-      ownerName: req.body.ownerName,
-      ownerAddress: req.body.address,
-      ownerPhone: req.body.ownerPhone,
-      additionalOne: req.body.additionalOne,
-      additionalTwo: req.body.additionalTwo,
-      spayed: req.body.spayed,
-      gender:req.body.dogGender,
-      neutered: req.body.neutered,
-      vaccinated: req.body.vaccinated,
-      kids: req.body.kids,
-      cats: req.body.cats,
-      dogs: req.body.dogs,
-      state: req.body.state,
-      city: req.body.city,
-      imagePath: result.url,
-    });
+    const post = new Post({randomNumber: Math.floor(Math.random()*1000000000000000),dogName: req.body.dogName,breed: req.body.breed,duration: req.body.type,date: req.body.postDate,dogAge: req.body.dogAge,adopted: false,shots: req.body.shots,ownerName: req.body.ownerName,ownerAddress: req.body.address,ownerPhone: req.body.ownerPhone,additionalOne: req.body.additionalOne,additionalTwo: req.body.additionalTwo,spayed: req.body.spayed,gender:req.body.dogGender,neutered: req.body.neutered,vaccinated: req.body.vaccinated,kids: req.body.kids,cats: req.body.cats, dogs: req.body.dogs,state: req.body.state,city: req.body.city,imagePath: result.url});
     post.save(function(err,result){
       if (!err){
         const postId= result._id;
@@ -343,55 +244,17 @@ app.get("/thank-you/:ranNum/:postId",requiresAuth(), function(req, res){
   const requestedPostId = req.params.postId;
   const ranNum = req.params.ranNum;
   Post.findOne({_id: requestedPostId}, function(err, post){
-    res.render("thank-you", {
-      text: req.oidc.isAuthenticated() ? 'LOGOUT' : 'LOGIN',
-      dogName: post.dogName,
-      ranNum:ranNum,
-      breed: post.breed,
-      ownerName: post.ownerName,
-      ownerPhone: post.ownerPhone,
-      additionalOne: post.additionalOne,
-      dogAge: post.dogAge,
-      gender:post.gender,
-      adopted: post.adopted,
-      imagePath: post.imagePath,
-      _id: requestedPostId
-    });
+    res.render("thank-you", {text: req.oidc.isAuthenticated() ? 'LOGOUT' : 'LOGIN',dogName: post.dogName, ranNum:ranNum,breed: post.breed,ownerName: post.ownerName,ownerPhone: post.ownerPhone,additionalOne: post.additionalOne,dogAge: post.dogAge, gender:post.gender,adopted: post.adopted,imagePath: post.imagePath,_id: requestedPostId});
   });
 });
 
 app.get("/posts/:postId/", requiresAuth(), function(req, res){
-
 const requestedPostId = req.params.postId;
-
   Post.findOne({_id: requestedPostId}, function(err, post){
-    res.render("post", {
-      text: req.oidc.isAuthenticated() ? 'LOGOUT' : 'LOGIN',
-      dogName: post.dogName,
-      date: post.date,
-      duration: post.duration,
-      breed: post.breed,
-      ownerName: post.ownerName,
-      ownerAddress: post.ownerAddress,
-      ownerPhone: post.ownerPhone,
-      additionalOne: post.additionalOne,
-      additionalTwo: post.additionalTwo,
-      dogAge: post.dogAge,
-      spayed: post.spayed,
-      neutered: post.neutered,
-      vaccinated: post.vaccinated,
-      kids: post.kids,
-      shots: post.shots,
-      gender:post.gender,
-      cats: post.cats,
-      dogs: post.dogs,
-      state: post.state,
-      city: post.city,
-      imagePath: post.imagePath,
-      _id: requestedPostId
-    });
+    res.render("post", { text: req.oidc.isAuthenticated() ? 'LOGOUT' : 'LOGIN', dogName: post.dogName,date: post.date, duration: post.duration, breed: post.breed, ownerName: post.ownerName, ownerAddress: post.ownerAddress, ownerPhone: post.ownerPhone, additionalOne: post.additionalOne, additionalTwo: post.additionalTwo, dogAge: post.dogAge, spayed: post.spayed, neutered: post.neutered, vaccinated: post.vaccinated, kids: post.kids, shots: post.shots, gender:post.gender, cats: post.cats, dogs: post.dogs, state: post.state, city: post.city, imagePath: post.imagePath, _id: requestedPostI});
   });
 });
+
 app.get("/terms-of-service", function (req, res) {
 res.render("tos",{
   text: req.oidc.isAuthenticated() ? 'LOGOUT' : 'LOGIN',
@@ -400,7 +263,6 @@ res.render("tos",{
 
 app.get('/experience-adoption', function(req,res){
     const axios = require("axios");
-  
   const options = {
     method: 'GET',
     url: 'https://dog-facts2.p.rapidapi.com/facts',
@@ -409,11 +271,7 @@ app.get('/experience-adoption', function(req,res){
       'X-RapidAPI-Host': 'dog-facts2.p.rapidapi.com'
     }
   };
-  
-  
   var noMatch = null;
-  
-  
       if(req.query.city || req.query.state) {
           const city = new RegExp(escapeRegex(req.query.city), 'gi');
           const state= new RegExp(escapeRegex(req.query.state), 'gi');
@@ -464,30 +322,9 @@ app.get('/experience-adoption', function(req,res){
   });
 
 app.get("/adopted/posts/:postId/", requiresAuth(), function(req, res){
-
   const requestedPostId = req.params.postId;
-  
     Post.findOne({_id: requestedPostId}, function(err, post){
-      res.render("adopted-post", {
-        dogName: post.dogName,
-        text: req.oidc.isAuthenticated() ? 'LOGOUT' : 'LOGIN',
-        date: post.date,
-        duration: post.duration,
-        breed: post.breed,
-        additionalOne: post.additionalOne,
-        additionalTwo: post.additionalTwo,
-        dogAge: post.dogAge,
-        spayed: post.spayed,
-        neutered: post.neutered,
-        vaccinated: post.vaccinated,
-        kids: post.kids,
-        shots: post.shots,
-        gender:post.gender,
-        cats: post.cats,
-        dogs: post.dogs,
-        imagePath: post.imagePath,
-        _id: requestedPostId
-      });
+      res.render("adopted-post", {dogName: post.dogName, text: req.oidc.isAuthenticated() ? 'LOGOUT' : 'LOGIN', date: post.date, duration: post.duration, breed: post.breed, additionalOne: post.additionalOne, additionalTwo: post.additionalTwo, dogAge: post.dogAge, spayed: post.spayed, neutered: post.neutered, vaccinated: post.vaccinated, kids: post.kids, shots: post.shots, gender:post.gender, cats: post.cats, dogs: post.dogs, imagePath: post.imagePath, _id: requestedPostI});
     });
   });
 
@@ -495,60 +332,14 @@ app.get("/:postId/edit/:ranNum", requiresAuth(), function (req, res) {
   const requestedPostId = req.params.postId;
   
   Post.findOne({_id: requestedPostId}, function(err, post){
-    res.render("edit", {
-      dogName: post.dogName,
-      text: req.oidc.isAuthenticated() ? 'LOGOUT' : 'LOGIN',
-      date: post.date,
-      duration: post.duration,
-      breed: post.breed,
-      ownerName: post.ownerName,
-      ownerAddress: post.ownerAddress,
-      ownerPhone: post.ownerPhone,
-      additionalOne: post.additionalOne,
-      additionalTwo: post.additionalTwo,
-      dogAge: post.dogAge,
-      spayed: post.spayed,
-      neutered: post.neutered,
-      vaccinated: post.vaccinated,
-      kids: post.kids,
-      adopted: post.adopted,
-      shots: post.shots,
-      gender:post.gender,
-      cats: post.cats,
-      dogs: post.dogs,
-      state: post.state,
-      city: post.city,
-      imagePath: post.imagePath,
-      _id: requestedPostId
-    });
+    res.render("edit", {dogName: post.dogName, text: req.oidc.isAuthenticated() ? 'LOGOUT' : 'LOGIN', date: post.date, duration: post.duration, breed: post.breed, ownerName: post.ownerName, ownerAddress: post.ownerAddress, ownerPhone: post.ownerPhone, additionalOne: post.additionalOne, additionalTwo: post.additionalTwo, dogAge: post.dogAge, spayed: post.spayed, neutered: post.neutered, vaccinated: post.vaccinated, kids: post.kids, adopted: post.adopted, shots: post.shots, gender:post.gender, cats: post.cats, dogs: post.dogs, state: post.state, city: post.city, imagePath: post.imagePath, _id: requestedPostI});
   });
-
 });
 
 // route to handle updates
 app.put('/:postId/edit',requiresAuth(), function (req, res) {
-      const requestedPostId = req.params.postId;
-      dogName= req.body.dogName,
-      adopted= req.body.adopted,
-      duration= req.body.type,
-      breed= req.body.breed,
-      ownerName= req.body.ownerName,
-      ownerAddress= req.body.address,
-      ownerPhone= req.body.ownerPhone,
-      additionalOne= req.body.additionalOne,
-      additionalTwo= req.body.additionalTwo,
-      dogAge= req.body.dogAge,
-      spayed= req.body.spayed,
-      neutered= req.body.neutered,
-      vaccinated= req.body.vaccinated,
-      kids= req.body.kids,
-      shots= req.body.shots,
-      gender=req.body.dogGender,
-      cats= req.body.cats,
-      dogs= req.body.dogs,
-      state= req.body.state,
-      city= req.body.city,
-      _id= requestedPostId,
+      const requestedPostId = req.params.postId; dogName= req.body.dogName,
+adopted= req.body.adopted, duration= req.body.type, breed= req.body.breed, ownerName= req.body.ownerName, ownerAddress= req.body.address, ownerPhone= req.body.ownerPhone, additionalOne= req.body.additionalOne, additionalTwo= req.body.additionalTwo, dogAge= req.body.dogAge, spayed= req.body.spayed, neutered= req.body.neutered, vaccinated= req.body.vaccinated, kids= req.body.kids, shots= req.body.shots, gender=req.body.dogGender, cats= req.body.cats, dogs= req.body.dogs, state= req.body.state,city= req.body.city, _id= requestedPostId,
       
   Post.updateOne({_id: requestedPostId}, {$set:{dogName:dogName,duration:duration,
     city:city,state:state,adopted:adopted,dogs:dogs,kids:kids,cats:cats,gender:gender,
@@ -564,7 +355,6 @@ app.put('/:postId/edit',requiresAuth(), function (req, res) {
   })
   })
 
-  
 app.get("/delete/:postId/:ranNum",requiresAuth(),function(req,res){
   Post.deleteOne({_id: req.params.postId},function(err){
     if(err){
@@ -575,16 +365,12 @@ app.get("/delete/:postId/:ranNum",requiresAuth(),function(req,res){
 });
 });
 
-
-
 app.get('/donation', requiresAuth(), function(req, res){
     res.render('donation', {
       key: Publishable_Key,
       text: req.oidc.isAuthenticated() ? 'LOGOUT' : 'LOGIN',
    })
 })
-
-
 
 app.post('/donation',requiresAuth(), function(req, res){
 
@@ -622,7 +408,6 @@ app.get('/breed', function(req, res){
   if(req.query.breed) {
     const breed = new RegExp(escapeRegex(req.query.breed), 'gi');
     const breedAPI=  new RegExp(spaceReplace(req.query.breed));
-
     const options = {
       method: 'GET',
       url: 'https://dog-breeds2.p.rapidapi.com/dog_breeds/breed'+breedAPI,
@@ -631,7 +416,6 @@ app.get('/breed', function(req, res){
         'X-RapidAPI-Host': 'dog-breeds2.p.rapidapi.com'
       }
     };
-    
     Post.find({$and:[{breed:breed},{adopted:false}]}, function(err, posts){
        if(err){
            console.log(err);
@@ -652,25 +436,10 @@ app.get('/breed', function(req, res){
             const nickname= response.data[0].meta.other_names
             const color= response.data[0].meta.colour
             console.log(response.data);
-            res.render("breed", {
-              posts: posts,
-              text: req.oidc.isAuthenticated() ? 'LOGOUT' : 'LOGIN',
-              noMatch: noMatch,
-              breed: breed,
-              origin: origin,
-              wikipedia: wikipedia,
-              image: image,
-              height: height,
-              weight: weight,
-              coat: coat,
-              life: life,
-              nickname: nickname,
-              color: color,
-              });   
+            res.render("breed", {posts: posts, text: req.oidc.isAuthenticated() ? 'LOGOUT' : 'LOGIN', noMatch: noMatch, breed: breed, origin: origin, wikipedia: wikipedia,image: image, height: height, weight: weight, coat: coat, life: life, nickname: nickname, color: color, });   
          }).catch(function (error) {
           console.error(error);
         });
-         
        }
       }).sort({date:"desc"});
 } else {
@@ -678,8 +447,6 @@ app.get('/breed', function(req, res){
         res.redirect('/breed?breed=Indian+pariah+dog')
 }
   })
-
-
 
 function escapeRegex(text) {
   return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
@@ -692,8 +459,6 @@ function spaceReplace(text) {
 setInterval(() => {
   http.get("https://woofyverse.herokuapp.com/");
 }, 25 * 60 * 1000); // every 25 minutes
-
-
 
 app.listen(port, function() {
   console.log("Server started sucessfully");
